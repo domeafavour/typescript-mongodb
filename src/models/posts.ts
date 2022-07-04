f4319b6909a86139ffaa4dba82c64e6d04eb21b8
zhanghaojie <1455123087@qq.com>
2022-07-04 18:12:09 +0800

    feat(models): posts models

---
+++ src/models/posts.ts
@@ -0,0 +1,46 @@
+import { ObjectID } from 'bson';
+import { client } from '../connectMongoDb';
+import { IPost, IUser } from '../typings';
+
+const db = client.db('test');
+
+export async function findAll() {
+  const cursor = db.collection('posts').find({});
+  const posts = await cursor.toArray();
+  return posts;
+}
+
+export async function findPost(id: string) {
+  const post = await db.collection('posts').findOne<IPost>({
+    _id: new ObjectID(id),
+  });
+
+  const commentsCursor = db
+    .collection('comments')
+    .aggregate<IPost & { users: IUser[] }>([
+      {
+        $lookup: {
+          localField: 'userId',
+          from: 'users',
+          foreignField: '_id',
+          as: 'users',
+        },
+      },
+      {
+        $project: {
+          title: 1,
+          users: 1,
+        },
+      },
+    ]);
+
+  const comments = await commentsCursor.toArray();
+
+  return {
+    ...post,
+    comments: comments.map((comment) => ({
+      title: comment.title,
+      user: comment.users[0],
+    })),
+  };
+}
--- src/routes/views.ts
+++ src/routes/views.ts
@@ -1,17 +1,12 @@
-import { ObjectID } from 'bson';
 import Router from 'koa-router';
-import { client } from '../connectMongoDb';
-import { IPost, IUser } from '../typings';
+import { findAll, findPost } from '../models/posts';
 
 const router = new Router({
   prefix: '/views/',
 });
 
-const db = client.db('test');
-
 router.get('posts', async (ctx) => {
-  const cursor = db.collection('posts').find({});
-  const posts = await cursor.toArray();
+  const posts = await findAll();
 
   await ctx.render('posts', {
     title: 'Posts',
@@ -20,40 +15,11 @@ router.get('posts', async (ctx) => {
 });
 
 router.get('posts/:id', async (ctx) => {
-  const post = await db.collection('posts').findOne<IPost>({
-    _id: new ObjectID(ctx.params.id),
-  });
-
-  const commentsCursor = db
-    .collection('comments')
-    .aggregate<IPost & { users: IUser[] }>([
-      {
-        $lookup: {
-          localField: 'userId',
-          from: 'users',
-          foreignField: '_id',
-          as: 'users',
-        },
-      },
-      {
-        $project: {
-          title: 1,
-          users: 1,
-        },
-      },
-    ]);
-
-  const comments = await commentsCursor.toArray();
+  const post = await findPost(ctx.params.id);
 
   await ctx.render('post-detail', {
     title: post?.title,
-    post: {
-      ...post,
-      comments: comments.map((comment) => ({
-        title: comment.title,
-        user: comment.users[0],
-      })),
-    },
+    post,
   });
 });
 
