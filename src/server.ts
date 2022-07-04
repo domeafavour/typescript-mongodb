10df710e54567dced980fb9701afcab4411908d9
zhanghaojie <1455123087@qq.com>
2022-07-04 11:10:21 +0800

    feat: "Adding Our First Endpoint"

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,35 @@
-console.log('Hello');
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import cors from 'koa2-cors';
+import KoaLogger from 'koa-logger';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+const PORT = process.env.PORT || 7645;
+
+router.get('/', async (ctx) => {
+  try {
+    ctx.body = {
+      status: 'success',
+    };
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.use(bodyParser());
+app.use(cors({ origin: '*' }));
+app.use(KoaLogger());
+app.use(router.routes());
+
+const server = app
+  .listen(PORT, async () => {
+    console.log(`Server listening on port: ${PORT}`);
+  })
+  .on('error', (err) => {
+    console.error(err);
+  });
+
+export default server;
