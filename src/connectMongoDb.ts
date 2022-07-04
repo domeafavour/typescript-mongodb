777d2a4f00e95d75811be5517193028ca8cab7e2
zhanghaojie <1455123087@qq.com>
2022-07-04 18:59:04 +0800

    feat: getCollection

--- src/connectMongoDb.ts
+++ src/connectMongoDb.ts
@@ -1,7 +1,14 @@
-import { MongoClient } from 'mongodb';
+import { CollectionOptions, Document, MongoClient } from 'mongodb';
 import { config } from './config';
 
 export const client = new MongoClient(config.mongoClientUrl);
+
+export function getCollection<TSchema extends Document>(
+  name: string,
+  options?: CollectionOptions
+) {
+  return client.db('test').collection<TSchema>(name, options);
+}
 
 async function connectMongoDb() {
   try {
