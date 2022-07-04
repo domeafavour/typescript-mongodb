4b09801e9479e0c8e3aedd91c0754f7b43edc528
zhanghaojie <1455123087@qq.com>
2022-07-04 13:23:14 +0800

    feat(config): add "mongoClientUrl"

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 interface IConfig {
   port: number;
+  mongoClientUrl: string;
 }
 
 export const config: IConfig = {
   port: parseInt(process.env.PORT!) || 7645,
+  mongoClientUrl: 'mongodb://localhost:27017',
 };
