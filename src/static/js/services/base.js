fb14b5544f59b6a1eafa266595a3236c1d450b0d
zhanghaojie <1455123087@qq.com>
2022-07-11 08:11:44 +0800

    feat(static): Remove current user from localStorage when login expired

--- src/static/js/services/base.js
+++ src/static/js/services/base.js
@@ -1,6 +1,11 @@
+import { removeCurrentUser } from '../utils/user.js';
+
 export async function fetchJsonData(url, options) {
   const res = await fetch(url, options);
   const json = await res.json();
+  if (json.code === 99999) {
+    removeCurrentUser();
+  }
   return json.data;
 }
 
