5eb4de295d84c27b96d5669f0656bd599f34969d
zhanghaojie <1455123087@qq.com>
2022-07-11 15:10:30 +0800

    style(static): Show comments count on posts page

--- src/static/js/components/Posts.js
+++ src/static/js/components/Posts.js
@@ -3,44 +3,43 @@ import ItemAvatar from './ItemAvatar.js';
 
 export default {
   template: `
-    <div style="position: relative">
-      <v-btn
-        fixed
-        dark
-        fab
-        bottom
-        right
-        color="pink"
-        @click="$router.push('/posts/add')"
-      >
-        <v-icon>mdi-pencil</v-icon>
-      </v-btn>
-      <v-card>
-        <v-list>
-          <template v-for="(post, index) in posts">
-            <v-list-item two-line :key="post.id" link @click="viewPost(post)">
-              <item-avatar :user="post.author" />
-              <v-list-item-content>
-                <v-list-item-title link>{{post.title}}</v-list-item-title>
-                <v-list-item-subtitle>{{post.description}}</v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-row>
-                  <v-col v-if="$global.user.id === post.author.id">
-                    <v-btn color="success" @click="editPost(post)">edit</v-btn>
-                  </v-col>
-                </v-row>
-              </v-list-item-action>
-            </v-list-item>
-            <v-divider
-              v-if="index < posts.length - 1"
-              :key="post.path"
-              inset
-            />
-          </template>
-        </v-list>
-      </v-card>
-    </div>
+  <div style="position: relative">
+    <v-btn
+      fixed
+      dark
+      fab
+      bottom
+      right
+      color="pink"
+      @click="$router.push('/posts/add')"
+    >
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
+    <v-card>
+      <v-list>
+        <template v-for="(post, index) in posts">
+          <v-list-item two-line :key="post.id" link @click="viewPost(post)">
+            <item-avatar :user="post.author" />
+            <v-list-item-content>
+              <v-list-item-title>{{ post.title }}</v-list-item-title>
+              <v-list-item-subtitle v-text="post.description" />
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-list-item-action-text>{{ post.commentsCount }} comments</v-list-item-action-text>
+              <v-icon
+                v-if="$global.user.id === post.author.id"
+                color="success lighten-1"
+                @click="editPost(post)"
+              >
+                mdi-pencil
+              </v-icon>
+            </v-list-item-action>
+          </v-list-item>
+          <v-divider v-if="index < posts.length - 1" :key="post.path" inset />
+        </template>
+      </v-list>
+    </v-card>
+  </div>
   `,
   components: {
     ItemAvatar,
