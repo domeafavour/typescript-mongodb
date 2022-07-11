17b966a9c6a4e1e4a3a1dcb9cbe9370d2a60fc85
zhanghaojie <1455123087@qq.com>
2022-07-11 13:52:05 +0800

    feat(static): Show posts of login user s on welcome page

--- src/static/js/components/Welcome.js
+++ src/static/js/components/Welcome.js
@@ -1,4 +1,32 @@
+import { render } from './../utils/markdown.js';
+
 export default {
   name: 'welcome',
-  template: `<h1>WELCOME, {{$global.user.name}}</h1>`,
+  template: `
+  <v-row dense>
+    <v-col cols="12" v-for="post in posts">
+      <v-card>
+        <v-card-title v-text="post.title" />
+        <v-card-subtitle v-text="post.description" />
+        <v-card-text v-html="post.html" />
+        <v-card-actions>
+          <v-btn text color="green" @click="$router.push('/posts/' + post.id + '/edit')">
+            edit
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+  `,
+  computed: {
+    loading() {
+      return !this.$global.user.id;
+    },
+    posts() {
+      return this.$global.user.posts.map((post) => ({
+        ...post,
+        html: render(post.content),
+      }));
+    },
+  },
 };
