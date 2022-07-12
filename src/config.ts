1b28c4aca64d96ef60171eb16de34551d09dd86e
zhanghaojie <1455123087@qq.com>
2022-07-12 17:15:20 +0800

    feat: Move `uploadDir` to where the static files are

--- src/config.ts
+++ src/config.ts
@@ -9,5 +9,5 @@ interface IConfig {
 export const config: IConfig = {
   port: parseInt(process.env.PORT!) || 7645,
   mongoClientUrl: 'mongodb://localhost:27017',
-  uploadDir: path.join(__dirname, '..', 'files'),
+  uploadDir: path.join(__dirname, 'static', 'files'),
 };
