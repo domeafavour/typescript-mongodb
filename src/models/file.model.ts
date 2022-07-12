a603e3c1fa0e13ac8e18836d841576ee476603f6
zhanghaojie <1455123087@qq.com>
2022-07-12 17:52:18 +0800

    feat(models): Add `FileVo` in file.model

--- src/models/file.model.ts
+++ src/models/file.model.ts
@@ -1,5 +1,34 @@
 import mongoose from 'mongoose';
 
+/**
+ * ```
+ *  {
+ *    id: {
+ *      $toString: '$_id',
+ *    },
+ *    _id: false,
+ *    url: {
+ *      $replaceOne: {
+ *        input: '$filepath',
+ *        find: `${your upload dir}`,
+ *        replacement: '',
+ *      },
+ *    },
+ *    fileName: '$originalFilename',
+ *    mimetype: true,
+ *    size: true,
+ *  }
+ * ```
+ */
+
+export type FileVo = {
+  id: string;
+  size: number;
+  mimetype: string;
+  url: string;
+  fileName: string;
+};
+
 export const fileSchema = new mongoose.Schema({
   filepath: String,
   newFilename: String,
