ecc58cd530721164a04bbfa9d26f64b63976e200
zhanghaojie <1455123087@qq.com>
2022-07-12 16:00:10 +0800

    feat(config): Add `uploadDir` config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
+import path from 'path';
+
 interface IConfig {
   port: number;
   mongoClientUrl: string;
+  uploadDir: string;
 }
 
 export const config: IConfig = {
   port: parseInt(process.env.PORT!) || 7645,
   mongoClientUrl: 'mongodb://localhost:27017',
+  uploadDir: path.join(__dirname, '..', 'files'),
 };
