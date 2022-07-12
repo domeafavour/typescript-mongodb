65ad2678c081985caea158b89fc912c1a82345b2
zhanghaojie <1455123087@qq.com>
2022-07-12 18:25:26 +0800

    feat(middlewares): Keep ext name by setting `keepExtensions` instead of handling it myself

--- src/middlewares/body-parser.ts
+++ src/middlewares/body-parser.ts
@@ -1,16 +1,10 @@
 import KoaBody from 'koa-body';
-import path from 'path';
 import { config } from '../config';
 
 export const bodyParser = KoaBody({
   formidable: {
     uploadDir: config.uploadDir,
-    async onFileBegin(name, file) {
-      file.filepath = file.filepath.replace(
-        new RegExp(`[^${path.sep}]+$`),
-        file.newFilename + path.extname(file.originalFilename!)
-      );
-    },
+    keepExtensions: true,
   },
   urlencoded: true,
   multipart: true,
